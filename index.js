2c72cd42f2ce89e070b68f1e9b05f1d97d6ca7da
erika91mtz <erikagusto19@gmail.com>
2023-06-15 10:24:19 -0600

    Index.JS
    created index.js file, copy and paste code

---
+++ index.js
@@ -0,0 +1,91 @@
+function formatDate(date) {
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#time-now");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#location").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#location-form");
+form.addEventListener("submit", handleSubmit);
+
+function convertToC(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temperature");
+  let temperature = tempElement.innerHTML;
+  temperature = Number(temperature);
+  tempElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+}
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToC);
+
+function searchLocation(position) {
+  let apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToF(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temperature");
+  let temperature = tempElement.innerHTML;
+  temperature = Number(temperature);
+  tempElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+let FahrenheitLink = document.querySelector("#fahrenheit-link");
+FahrenheitLink.addEventListener("click", convertToF);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Denver");
