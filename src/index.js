0455880e003aeb69857b06bd8a5fe4d96f0d04c3
erika91mtz <erikagusto19@gmail.com>
2023-06-19 17:29:56 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ let dateElement = document.querySelector("#time-now");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -69,6 +70,7 @@ function getForecast(coordinates) {
   let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function searchCity(city) {
@@ -121,4 +123,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Denver");
-displayForecast();
