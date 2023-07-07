e591fe1420dafd8161bc304bf293391d003e7a83
erika91mtz <erikagusto19@gmail.com>
2023-07-07 11:21:33 -0600

    console.log

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
